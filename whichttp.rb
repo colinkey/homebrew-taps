ce547ed62293e0466b941d3d22d9ab270a0865a7
Colin Key <colin.m.key@gmail.com>
2021-12-21 20:32:31 -0700

    Remove configure from install

--- whichttp.rb
+++ whichttp.rb
@@ -14,7 +14,7 @@ class Whichttp < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "crystal", "build" "./src/whichttp.cr"
     bin.install "./whichttp"
