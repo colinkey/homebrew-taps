6229b8e06d1c9001b515156ac21d2f8d55524fbc
Colin Key <colin.m.key@gmail.com>
2021-12-29 19:56:02 -0700

    Update whichttp to 0.2.1

--- whichttp.rb
+++ whichttp.rb
@@ -1,8 +1,8 @@
 class Whichttp < Formula
   desc "CLI lookup tool for HTTP Status Codes"
   homepage "https://www.github.com/colinkey/whichttp"
-  url "https://github.com/colinkey/whichttp/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "28cbed0d1fadba10f1f9515c3668508f9b71ceed0cdd8aa374cf035449de983a"
+  url "https://github.com/colinkey/whichttp/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "48fdd3b415e2516ad7cbbb56b111f91a76ac3a8bedee321d4e792db34bc73812"
   license "MIT"
 
   depends_on "crystal" => :build
